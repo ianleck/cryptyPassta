49190230b62a3876670dd2babd779fbb3d9455dd
y1bs <39340177+y1bs@users.noreply.github.com>
2020-03-16 14:57:41 +0800

    Completed Updating Global.sol
    Need some coordination on methods with Passport but other than that it is more or less ok

--- src/blockchain/contracts/Global.sol
+++ src/blockchain/contracts/Global.sol
@@ -2,13 +2,16 @@ pragma solidity ^0.5.0;
 import "./Passport.sol";
 
 contract Global {
-    address public _globalOwner;
-    Passport passport;
+    address public _globalOwner = msg.sender;
+    Passport passport;    
 
-    mapping(address => Worker) worker;
-    mapping(string => TransferRecords) transferAuthority;
+    //Maps address of worker accounts
+    mapping(address => Worker) workers;
 
-    struct TransferRecords {
+    //Maps UUID of passports to trasnfer Records
+    mapping(string => TransferRecord) transferAuthority;
+
+    struct TransferRecord {
         address prevOwner;
         address[] newOwner;
         bool isPending;
@@ -16,66 +19,63 @@ contract Global {
 
     struct Worker {
         string name;
-        const string nationality;
+        const address nationality; //Linked to Country minter
         bool isActive;
     }
 
-
-    function addNewCountry() public onlyGlobalOwner() {}
-
-    function addNewWorker(string name) public onlyMinter() {
-        string nationality = passport.viewMinterCountry(msg.sender);
-        Worker memory newWorker = Worker(name, nationality, true);
+    function addNewWorker(string name, address workerAddress) public onlyMinter() {
+        Worker memory newWorker = Worker(name, msg.sender, true);
+        workers[workerAddress] = newWorker;
     }
 
     function updateWorkerStatus(address workerAddress, string name, bool status) public onlyMinter() {
-        require(passport.viewMinterCountry(msg.sender) == worker[workerAddress].nationality, "Cannot update status of worker from another country");
-        worker[workerAddress].name = name ;
-        worker[workerAddress].isActive = status ;
-    }
-
-    function acceptTraveler(string UUID, uint256 date) public onlyWorker() {
-       passport.addTravelHistory(worker[msg.sender].nationality, Enter, date);
-       transferAuthority[UUID].isPending = false ; 
-    }
-
-
-    function rejectTraveler(string UUID) public onlyWorker() {
+        require(workers[workerAddress].nationality == msg.sender, "Cannot update status of worker from another country");
         
+        worker[workerAddress].name = name;
+        worker[workerAddress].isActive = status;
     }
 
-    function retrievePassportDetails(string UUID) public onlyWorker() {
+    //Precondition: Date is in UNIX epoch seconds format
+    //PostCondition: Create a transfer Record, Add new travel record to passport
+    function travelerDeparture(string UUID, uint256 date, address[] travelList) public onlyWorker(){
+        
+        address[] travels = travelList;
+        TransferRecord memory newTransfer = TransferRecord(msg.sender, travels, true);
+        transferAuthority[UUID] = newTransfer;
 
+        passport.addTravelHistory(UUID, Exit, block.timestamp);
+    }
+
+    //Precondition: Date is in UNIX epoch seconds format
+    //Postcondition: Freeze trasnfer record and add new travel record
+    function acceptTraveler(string UUID, uint256 date) public onlyWorker() {
+
+        transferAuthority[UUID].isActive = false;
+
+        passport.addTravelHistory(UUID, Entry, block.timestamp);
 
     }
+
+    //Precondition: transferRequest must exist
+    //PostCondition: Flip locations on transfer request
+    function rejectTraveler(string UUID) public onlyWorker() {
+
+        Address toLoc = transferAuthority[UUID].prevOwner;
+        transferAuthority[UUID].prevOwner = workers[msg.sender].nationality;
+        Address[] newList = [toLoc];
+        transferAuthority[UUID].newOwner = newList;
+
+    }
+
 
     modifier onlyMinter() {
-      require()
+      require(passport.checkOwner(msg.sender), "[Error] This is a minter only action")
+      _;
     }
 
-    modifier onlyWorker() {}
-
-    modifier onlyGlobalOwner() {
-        require(
-            msg.sender == _globalOwner,
-            "You do not have the authority to perform this action"
-        );
-    }
-}
-    function rejectTraveler() public onlyWorker() {}
-
-    function retrievePassportDetails() public onlyWorker() {}
-
-    modifier onlyMinter() {
-      require()
+    modifier onlyWorker() {
+        require(workers[msg.sender].isActive == true, "[Error] This is an active worker only action")
+        _;
     }
 
-    modifier onlyWorker() {}
-
-    modifier onlyGlobalOwner() {
-        require(
-            msg.sender == _globalOwner,
-            "You do not have the authority to perform this action"
-        );
-    }
 }
