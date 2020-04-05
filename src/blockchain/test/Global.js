a43cda1a9a24bfc2e1ce99926f3b128f285f27ec
y1bs <39340177+y1bs@users.noreply.github.com>
2020-04-05 15:32:06 +0800

    Updated Global
    Change to access rights to allow  countries to check active worker and active travel Transfer

--- src/blockchain/test/Global.js
+++ src/blockchain/test/Global.js
@@ -1,16 +1,16 @@
-const TruffleAssert = require('truffle-assertions');
-const Passport = artifacts.require('./Passport.sol');
-const Global = artifacts.require('./Global.sol');
+const TruffleAssert = require("truffle-assertions");
+const Passport = artifacts.require("./Passport.sol");
+const Global = artifacts.require("./Global.sol");
 
-contract('Global', accounts => {
+contract("Global", (accounts) => {
   let globalInstance;
   let platformOwner = accounts[0];
   let countryAacc = accounts[1];
   let countryBacc = accounts[2];
   let workerA = accounts[3];
   let workerB = accounts[4];
-  let passportUUID1 = 'AAA123';
-  let passportUUID2 = 'BBB123';
+  let passportUUID1 = "AAA123";
+  let passportUUID2 = "BBB123";
 
   before(async () => {
     passportInstance = await Passport.deployed();
@@ -18,80 +18,80 @@ contract('Global', accounts => {
     //console.log("Global : " + globalInstance.address);
   });
 
-  it('Test case 1: Passport.sol deploys successfully', async () => {
+  it("Test case 1: Passport.sol deploys successfully", async () => {
     const address = passportInstance.address;
     assert.notEqual(address, 0x0);
-    assert.notEqual(address, '');
+    assert.notEqual(address, "");
     assert.notEqual(address, null);
     assert.notEqual(address, undefined);
     //console.log(passportInstance.address);
   });
 
-  it('Test case 2: Global.sol deploys successfully', async () => {
+  it("Test case 2: Global.sol deploys successfully", async () => {
     const address = globalInstance.address;
     assert.notEqual(address, 0x0);
-    assert.notEqual(address, '');
+    assert.notEqual(address, "");
     assert.notEqual(address, null);
     assert.notEqual(address, undefined);
     //console.log(globalInstance.address);
   });
 
-  it('Test case 3: Set up passports for testing', async () => {
+  it("Test case 3: Set up passports for testing", async () => {
     const registeredCountryA = await passportInstance.registerCountry(
       countryAacc,
-      'AAA',
+      "AAA",
       {
-        from: platformOwner
+        from: platformOwner,
       }
     );
     TruffleAssert.eventEmitted(
       registeredCountryA,
-      'countryRegistrationSuccess'
+      "countryRegistrationSuccess"
     );
 
     const registeredCountryB = await passportInstance.registerCountry(
       countryBacc,
-      'BBB',
+      "BBB",
       {
-        from: platformOwner
+        from: platformOwner,
       }
     );
     TruffleAssert.eventEmitted(
       registeredCountryB,
-      'countryRegistrationSuccess'
+      "countryRegistrationSuccess"
     );
 
     const aPassport = await passportInstance.createPassport(passportUUID1, {
-      from: countryAacc
+      from: countryAacc,
     });
-    TruffleAssert.eventEmitted(aPassport, 'passportCreationSuccess');
+    TruffleAssert.eventEmitted(aPassport, "passportCreationSuccess");
 
     const bPassport = await passportInstance.createPassport(passportUUID2, {
-      from: countryBacc
+      from: countryBacc,
     });
-    TruffleAssert.eventEmitted(bPassport, 'passportCreationSuccess');
+    TruffleAssert.eventEmitted(bPassport, "passportCreationSuccess");
   });
 
-  it('Test case 4: Register New worker under country A and country B', async () => {
+  it("Test case 4: Register New worker under country A and country B", async () => {
     //console.log("A address: " + workerA);
     //console.log(await passportInstance.checkVerifiedCountry(countryAacc));
-    let aWorker = await globalInstance.addNewWorker('workerAAA', workerA, {
-      from: countryAacc
+    let aWorker = await globalInstance.addNewWorker("workerAAA", workerA, {
+      from: countryAacc,
     });
     //console.log("Country A: " + countryAacc);
     //console.log("a log: " + aWorker);
-    TruffleAssert.eventEmitted(aWorker, 'workerRegistered');
+    TruffleAssert.eventEmitted(aWorker, "workerRegistered");
 
     //console.log("Here 2");
-    let bWorker = await globalInstance.addNewWorker('workerBBB', workerB, {
-      from: countryBacc
+    let bWorker = await globalInstance.addNewWorker("workerBBB", workerB, {
+      from: countryBacc,
     });
     //console.log("b log: " + bworker);
 
-    TruffleAssert.eventEmitted(bWorker, 'workerRegistered');
+    TruffleAssert.eventEmitted(bWorker, "workerRegistered");
   });
 
-  it('Test case 5: Send travellers out from both country A and B', async () => {
+  it("Test case 5: Send travellers out from both country A and B", async () => {
     //Tourist 1 leaves home country A to Country B and is processed by WorkerA
     try {
       let listforA = [countryBacc];
@@ -99,12 +99,12 @@ contract('Global', accounts => {
         passportUUID1,
         listforA,
         {
-          from: countryAacc
+          from: countryAacc,
         }
       );
       //console.log("A Departs: " + aDepart.eventEmitted);
 
-      TruffleAssert.eventEmitted(aDepart, 'departure');
+      TruffleAssert.eventEmitted(aDepart, "departure");
 
       let listforB = [countryAacc];
       //Tourist 2 leaves home country B to Country A and is processed by WorkerB
@@ -112,21 +112,25 @@ contract('Global', accounts => {
         passportUUID2,
         listforB,
         {
-          from: countryBacc
+          from: countryBacc,
         }
       );
 
       //console.log("B Departs: " + bDepart.eventEmitted);
 
-      TruffleAssert.eventEmitted(bDepart, 'departure');
+      TruffleAssert.eventEmitted(bDepart, "departure");
 
       let tx1 = await globalInstance.checkActiveTransfer(passportUUID1, {
-        from: platformOwner
+        from: countryAacc,
       });
 
+      //console.log("Tx1 = " + tx1);
+
       let tx2 = await globalInstance.checkActiveTransfer(passportUUID2, {
-        from: platformOwner
+        from: countryBacc,
       });
+
+      //console.log("Tx2 = " + tx2);
 
       let tx3 = tx1 && tx2;
 
@@ -134,65 +138,95 @@ contract('Global', accounts => {
       assert.equal(
         tx3,
         true,
-        'Did not successfully register departure of tourists'
+        "Did not successfully register departure of tourists"
       );
     } catch (e) {
       console.log(e);
     }
   });
 
-  it('Test case 6: Accept Tourist A into Country B', async () => {
+  it("Test case 6: Accept Tourist A into Country B", async () => {
     let aArrive = await globalInstance.acceptTraveler(passportUUID1, {
-      from: countryBacc //workerB
+      from: countryBacc, //workerB
     });
 
     let tx1 = await globalInstance.checkActiveTransfer(passportUUID1, {
-      from: platformOwner
+      from: countryBacc,
     });
 
     //check result
-    assert.equal(tx1, false, 'Did not successfully Accept Traveler');
-    TruffleAssert.eventEmitted(aArrive, 'arrival');
+    assert.equal(tx1, false, "Did not successfully Accept Traveler");
+    TruffleAssert.eventEmitted(aArrive, "arrival");
   });
 
-  it('Test case 7: Reject Tourist B from entering Country A', async () => {
+  it("Test case 7: Reject Tourist B from entering Country A", async () => {
     let bReject = await globalInstance.rejectTraveler(passportUUID2, {
-      from: countryAacc //workerA
+      from: countryAacc, //workerA
     });
 
     let tx1 = await globalInstance.checkActiveTransfer(passportUUID2, {
-      from: platformOwner
+      from: countryAacc,
     });
 
     //check result
-    assert.equal(tx1, true, 'Did not successfully reject traveler');
-    TruffleAssert.eventEmitted(bReject, 'rejection');
+    assert.equal(tx1, true, "Did not successfully reject traveler");
+    TruffleAssert.eventEmitted(bReject, "rejection");
   });
 
-  it('Test case 8: Tourist B returns to Country B', async () => {
+  it("Test case 8: Tourist B returns to Country B", async () => {
     let bArrive = await globalInstance.acceptTraveler(passportUUID2, {
-      from: countryBacc //workerB
+      from: countryBacc, //workerB
     });
 
     let tx1 = await globalInstance.checkActiveTransfer(passportUUID2, {
-      from: platformOwner
+      from: countryBacc,
     });
 
     //check result
-    assert.equal(tx1, false, 'Did not successfully accept returning traveler');
-    TruffleAssert.eventEmitted(bArrive, 'arrival');
+    assert.equal(tx1, false, "Did not successfully accept returning traveler");
+    TruffleAssert.eventEmitted(bArrive, "arrival");
   });
 
-  it('Test case 9: Deactivate worker B', async () => {
-    await globalInstance.updateWorkerStatus(workerB, 'BBB', false, {
-      from: countryBacc
+  it("Test case 9: Deactivate worker B", async () => {
+    await globalInstance.updateWorkerStatus(workerB, "BBB", false, {
+      from: countryBacc,
     });
 
     let tx1 = await globalInstance.checkActiveWorker(workerB, {
-      from: platformOwner
+      from: countryBacc,
     });
 
     //check result
-    assert.equal(tx1, false, 'Did not successfully deactivate worker');
+    assert.equal(tx1, false, "Did not successfully deactivate worker");
+  });
+
+  it("Test case 10: Should not be able to register worker if not country", async () => {
+    try {
+      let aWorker = await globalInstance.addNewWorker("fakeWorker", workerA, {
+        from: workerA,
+      });
+    } catch (error) {
+      //check result
+      assert.equal(
+        error.message,
+        "Returned error: VM Exception while processing transaction: revert [ERROR] No such country has been registered -- Reason given: [ERROR] No such country has been registered.",
+        "Did not successfully catch invalid country error for registering worker"
+      );
+    }
+  });
+
+  it("Test case 11: Should not be able to register worker if not country", async () => {
+    try {
+      let aWorker = await globalInstance.addNewWorker("fakeWorker", workerA, {
+        from: workerA,
+      });
+    } catch (error) {
+      //check result
+      assert.equal(
+        error.message,
+        "Returned error: VM Exception while processing transaction: revert [ERROR] No such country has been registered -- Reason given: [ERROR] No such country has been registered.",
+        "Did not successfully catch invalid country error for registering worker"
+      );
+    }
   });
 });
