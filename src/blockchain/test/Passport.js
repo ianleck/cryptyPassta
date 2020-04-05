219ba9815aa6b1c8e0de8e14cd325343ce31b72c
y1bs <39340177+y1bs@users.noreply.github.com>
2020-04-05 13:13:39 +0800

    Updated passport and passport test
    Updated to include viewRegisteredCountryList( ) in passport and included a test to check it out. Do note, some variables were added to ensure that it works.

--- src/blockchain/test/Passport.js
+++ src/blockchain/test/Passport.js
@@ -1,93 +1,104 @@
-const TruffleAssert = require('truffle-assertions');
-const Passport = artifacts.require('./Passport.sol');
+const TruffleAssert = require("truffle-assertions");
+const Passport = artifacts.require("./Passport.sol");
 
-contract('Passport', accounts => {
+contract("Passport", (accounts) => {
   let passportInstance;
-  const UUID = 'SGD123';
+  const UUID = "SGD123";
   const SGInstance = accounts[1];
 
   before(async () => {
     passportInstance = await Passport.deployed();
   });
 
-  it('deploys successfully', async () => {
+  it("Test case 1: deploys successfully", async () => {
     const address = passportInstance.address;
     assert.notEqual(address, 0x0);
-    assert.notEqual(address, '');
+    assert.notEqual(address, "");
     assert.notEqual(address, null);
     assert.notEqual(address, undefined);
   });
 
-  it('should add country successfully', async () => {
+  it("Test case 2: should add country successfully", async () => {
     try {
       await passportInstance.viewRegisteredCountry.call(SGInstance);
     } catch (err) {
       assert.ok(/revert/.test(err.message));
       assert.isTrue(
-        err.message.includes('[ERROR] No such country has been registered')
+        err.message.includes("[ERROR] No such country has been registered")
       );
       const registeredCountry = await passportInstance.registerCountry(
         SGInstance,
-        'SGD'
+        "SGD"
       );
       assert.equal(
         await passportInstance.viewRegisteredCountry.call(SGInstance),
-        'SGD'
+        "SGD"
       );
       TruffleAssert.eventEmitted(
         registeredCountry,
-        'countryRegistrationSuccess'
+        "countryRegistrationSuccess"
       );
-      TruffleAssert.eventEmitted(registeredCountry, 'MinterAdded');
+      TruffleAssert.eventEmitted(registeredCountry, "MinterAdded");
     }
   });
 
-  it('should add passport successfully', async () => {
+  it("Test case 3: should add passport successfully", async () => {
     try {
       await passportInstance.viewPassport(UUID);
     } catch (err) {
       assert.ok(/revert/.test(err.message));
       assert.isTrue(
-        err.message.includes('[ERROR] No such passport has been created')
+        err.message.includes("[ERROR] No such passport has been created")
       );
       const createPassport = await passportInstance.createPassport(UUID, {
-        from: SGInstance
+        from: SGInstance,
       });
       const retrievedPassport = await passportInstance.viewPassport(UUID);
-      const newPassport = [true, '0', SGInstance];
-      TruffleAssert.eventEmitted(createPassport, 'passportCreationSuccess');
+      const newPassport = [true, "0", SGInstance];
+      TruffleAssert.eventEmitted(createPassport, "passportCreationSuccess");
       assert.deepEqual(retrievedPassport, newPassport);
     }
   });
 
-  it('should be able to add travel records successfully', async () => {
+  it("Test case 4: should be able to add travel records successfully", async () => {
     const createdPassport = await passportInstance.viewPassport(UUID);
-    assert.equal(createdPassport.travelRecordLength, '0');
+    assert.equal(createdPassport.travelRecordLength, "0");
     const timestamp = Math.floor(Date.now() / 1000);
     const addTravelRecord = await passportInstance.addTravelRecord(
       UUID,
-      'EXIT',
+      "EXIT",
       timestamp,
       { from: SGInstance }
     );
-    TruffleAssert.eventEmitted(addTravelRecord, 'travelRecordAdditionSuccess');
+    TruffleAssert.eventEmitted(addTravelRecord, "travelRecordAdditionSuccess");
     const updatedPassport = await passportInstance.viewPassport(UUID);
-    assert.equal(updatedPassport.travelRecordLength, '1');
+    assert.equal(updatedPassport.travelRecordLength, "1");
     const retrievedTravelRecords = await passportInstance.viewPassportTravelRecords(
       UUID
     );
 
-    assert.equal(retrievedTravelRecords.length, '1');
-    const newRecord = [SGInstance, 'EXIT', timestamp.toString()];
+    assert.equal(retrievedTravelRecords.length, "1");
+    const newRecord = [SGInstance, "EXIT", timestamp.toString()];
     assert.deepEqual(retrievedTravelRecords[0], newRecord);
   });
 
-  it('should be able to freeze passport successfully', async () => {
+  it("Test case 5: should be able to freeze passport successfully", async () => {
     const freezePassport = await passportInstance.freezePassport(UUID, {
-      from: SGInstance
+      from: SGInstance,
     });
-    TruffleAssert.eventEmitted(freezePassport, 'freezePassportSuccess');
+    TruffleAssert.eventEmitted(freezePassport, "freezePassportSuccess");
     const updatedPassport = await passportInstance.viewPassport(UUID);
     assert.equal(updatedPassport.isActive, false);
+  });
+
+  it("Test case 6: should return countrylist successfully", async () => {
+    try {
+      let countrylist = await passportInstance.viewRegisteredCountryList();
+      //console.log(countrylist[0]);
+      //console.log(accounts[1]);
+      assert.equal(countrylist[0], accounts[1]);
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
