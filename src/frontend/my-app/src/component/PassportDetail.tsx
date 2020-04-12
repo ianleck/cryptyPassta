430e7ed17050752eae726fa12b6ed7240122f8f7
Yuan Rong <wangzi2807@gmail.com>
2020-04-12 11:21:57 +0800

    Add Travel Record to passport

--- src/frontend/my-app/src/component/PassportDetail.tsx
+++ src/frontend/my-app/src/component/PassportDetail.tsx
@@ -1,10 +1,30 @@
-import React, { useEffect } from 'react';
-import { EditOutlined } from '@ant-design/icons';
+import React, { useEffect } from "react";
+import { EditOutlined } from "@ant-design/icons";
+import * as PassportAPI from "../webservice/passport";
+import { message } from "antd";
+import moment from "moment";
 
-require('./passportDetail.scss');
+require("./passportDetail.scss");
 
 function PassportDetails(props: any) {
   const { passportInfo, country } = props;
+  const [countryList, setCountryList] = React.useState<string[]>([]);
+
+  // ----------------------- USEEFFECT------------------------
+  //Component Did Mount
+  useEffect(() => {
+    PassportAPI.getCountryList()
+      .then((res) => {
+        const formatted = Object.keys(res.data).reduce((ret: any, key: any) => {
+          ret[res.data[key]] = key;
+          return ret;
+        }, {});
+        console.log("FORMATTED", formatted);
+        setCountryList(formatted);
+      })
+      .catch((err) => message.error(err));
+  }, []);
+
   return (
     <React.Fragment>
       <div className="passport">
@@ -12,13 +32,13 @@ function PassportDetails(props: any) {
           <h4 className="title">PASSPORT</h4>
 
           <img
-            src={require('../images/Passport/passportProfile2.jpg')}
+            src={require("../images/Passport/passportProfile2.jpg")}
             height="350px"
           />
         </div>
         <div className="passport-details">
           <h4>
-            {country === 'SG' ? 'REPUBLIC OF SINGAPORE' : 'OTHER COUNTRY'}
+            {country === "SG" ? "REPUBLIC OF SINGAPORE" : "OTHER COUNTRY"}
           </h4>
           <p className="title">Passport No</p>
           <h5>{passportInfo.passportUUID}</h5>
@@ -30,6 +50,15 @@ function PassportDetails(props: any) {
           <h5>{passportInfo.ic}</h5>
           <p className="title">Address</p>
           <h5>{passportInfo.address}</h5>
+        </div>
+        <div className="passport-history">
+          <h4>Travel History</h4>
+          {passportInfo.travelRecord.map((r: any) => (
+            <p>
+              <span className="title">[{r[1]}]</span> {countryList[r[0]]}{" "}
+              {moment.unix(r[2]).format("MM/DD/YYYY")}
+            </p>
+          ))}
         </div>
       </div>
     </React.Fragment>
